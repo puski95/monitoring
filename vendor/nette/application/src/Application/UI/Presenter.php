3f9451711eb77c79fa4f673a57250cb002113cd0
Michal Puškár <michal.puskar@seznam.cz>
2025-08-28 13:17:07 +0200

    update

--- vendor/nette/application/src/Application/UI/Presenter.php
+++ vendor/nette/application/src/Application/UI/Presenter.php
@@ -296,6 +296,10 @@ abstract class Presenter extends Control implements Application\IPresenter
 	 */
 	protected function beforeRender()
 	{
+		$tag = shell_exec('git describe --tags --abbrev=0 2>/dev/null');
+    	$this->template->appVersion = $tag !== null
+        	? trim($tag)
+        	: 'alfa version';
 	}
 
 
