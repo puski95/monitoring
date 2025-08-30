73c67d139ef2a66fb1294818455d1cac15f38a79
Michal Puškár <michal.puskar@seznam.cz>
2025-08-30 10:22:13 +0200

    Update

---
+++ app/UI/Issues/IssuesPresenter.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\UI\Issues;
+
+use Nette;
+use Nette\Application\UI\Form;
+use App\Model\Config;
+
+final class IssuesPresenter extends Nette\Application\UI\Presenter
+{
+
+	private $id;
+
+    protected function startup()
+    {
+        parent::startup();
+        
+		$this->protected();
+    }
+
+	public function __construct(
+		private Nette\Database\Connection $database,
+        private Config $config
+	) {
+
+	}
+
+    public function renderDefault($id): void
+	{
+		$this->template->id = $id;
+
+	}
+
+	/**
+	 * FORMS
+	 */
+
+	protected function createComponentContactForm(): Form
+	{
+		$form = new Form;
+
+		$form->addSubmit('submit');
+		$form->onSuccess[] = [$this, 'contactFormProcess'];
+		return $form;
+	}
+
+	public function contactFormProcess(Form $form, $data): void
+	{
+
+		$values = $form->getValues();
+        
+
+	}
+    
+
+}
